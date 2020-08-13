f26eb4514a502bfedaadc0ec82d2a8bd76b1f5c3
root <root@centos1.unixdev.ca>
2020-08-13 17:49:35 -0400

    test

--- init.sh
+++ init.sh
@@ -24,7 +24,7 @@ fi
 if [[ -e "/opt/app/src/main.py" ]]; then
 	echo "Launch service"
 	cd /opt/app/src
-	exec uvicorn main:app --reload
+	exec uvicorn main:app --reload --port 80
 	#exec python3 ./main.py
 else 
 	echo "Wait for exit"
